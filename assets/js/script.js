37f12e7ec2cab63a708b18e6e5903f7e7dbf8f06
Cory Piette <coryjpiette@gmail.com>
2021-06-18 09:56:52 -0400

    first time (9am) shows up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function () {
+    $("#today").text(moment().format('dddd') + ", " + moment().format('LL'));
+    scheduleInput();
+});
+
+$("#appt9am").click(function () {
+    var availableSlot = "#9amFormat";
+    var apptInfo = document.getElementById('timeFormat9am').value;
+    setAppt(availableSlot, apptInfo);
+    scheduleInput();
+});
