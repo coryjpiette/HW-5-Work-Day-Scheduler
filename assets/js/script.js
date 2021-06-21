22c0cc671940b300e3a47ab4bff6ee9f1c8d74c4
Cory Piette <coryjpiette@gmail.com>
2021-06-21 13:23:46 -0400

    adding time resets and formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,9 @@ $("#confirmButton10am").click(function () {
     scheduleInput();
 });
 
-$("#confirmButton10am").click(function () {
-    var availableSlot = "textarea10am";
-    var apptInfo = document.getElementById('textarea10am').value;
+$("#confirmButton11am").click(function () {
+    var availableSlot = "textarea11am";
+    var apptInfo = document.getElementById('textarea11am').value;
     setAppt(availableSlot, apptInfo);
     scheduleInput();
 });
@@ -40,7 +40,7 @@ scheduleArray.push(newAppt);
 
 localStorage.setItem('scheduleArray', JSON.stringify(scheduleArray));
 
-function writeToSchedule() {
+function scheduleRecord() {
     scheduleArray = JSON.parse(localStorage.getItem("scheduleArray")) || [];
 
     scheduleArray.map(newAppt => {
@@ -50,3 +50,26 @@ function writeToSchedule() {
         }
     });
 }
+var scheduleArray = [];
+var time = moment();
+var timeFormat = 'hh:mma';
+var startTime = moment('12:00am', format);
+var ninestartTime = moment('9:00am', format);
+var nineEndTime = moment('10:00am', format);
+var tenstartTime = moment('10:00am', format);
+var tenEndTime = moment('11:00am', format);
+var elevenstartTime = moment('11:00am', format);
+var elevenEndTime = moment('11:59am', format);
+var twelvestartTime = moment('12:00pm', format);
+var twelveEndTime = moment('12:59pm', format);
+var onestartTime = moment('1:00pm', format);
+var oneEndTime = moment('1:59pm', format);
+var twostartTime = moment('2:00pm', format);
+var twoEndTime = moment('2:59pm', format);
+var threestartTime = moment('3:00pm', format);
+var threeEndTime = moment('3:59pm', format);
+var fourstartTime = moment('4:00pm', format);
+var fourstartTime = moment('4:59pm', format);
+var fivestartTime = moment('5:00pm', format);
+var fiveEndTime = moment('5:59pm', format);
+
